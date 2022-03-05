2c2c2d31f5d44810c64054d4b54c45203df623f4
thebig4accountant <info@thebig4accountant.com>
2022-03-05 07:04:40 -0500

    Video: 23 min mark

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,12 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/status/200/"
 endpoint = "https://httpbin.org/anything"
 
 
 # Application programming interface
 
-get_response = requests.get(endpoint) # HTTP Request
-print(get_response.text) # print raw text response
+get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
+print(get_response.json) # print raw text response
+
+print(get_response.json())
