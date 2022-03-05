0cff891ff194706c8360e4299c57582a6bd25af4
thebig4accountant <info@thebig4accountant.com>
2022-03-05 12:16:30 -0500

    Video: 1:01 hour

---
+++ backend/api/views.py
@@ -0,0 +1,10 @@
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from products.models import Product
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data)
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
 
+get_response = requests.get(endpoint, json={"product_id": 123})
 
-# Application programming interface
-
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
-print(get_response.json) # print raw text response
 
 print(get_response.json())
