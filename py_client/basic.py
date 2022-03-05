c3c2c3b566185bea339a991bd00fe9bb8728d43c
thebig4accountant <info@thebig4accountant.com>
2022-03-05 06:30:24 -0500

    Initial commit

---
+++ py_client/basic.py
@@ -0,0 +1,10 @@
+import requests
+
+endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/anything"
+
+
+# Application programming interface
+
+get_response = requests.get(endpoint) # HTTP Request
+print(get_response.text) # print raw text response
