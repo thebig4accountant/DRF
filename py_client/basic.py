95db1c71221efb95dd7be1a08865734b9ca59386
thebig4accountant <info@thebig4accountant.com>
2022-03-06 06:03:35 -0500

    Completed - 12: Injest data with Django RFW views

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": 123})
+get_response = requests.post(endpoint, json={"title": "ABC123", "content": "Hello world", "price": 13.22})
 
 
 print(get_response.json())
